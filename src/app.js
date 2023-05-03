8df55e10a1faeac15e714d1761c2a003b47b564f
Karen_Mora <momk2709@gmail.com>
2023-05-03 11:11:05 -0600

    Sesion 2

--- src/app.js
+++ src/app.js
@@ -3,6 +3,30 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.json());
+
+var baseDeDatos = [
+  {
+    nombre: "Emma",
+    email: "ejemplo@gmail.com",
+    contrasena: "perrito123",
+  },
+];
+
+app.post("/auth/register", (req, res) => {
+  try {
+    const { nombre, apellido, email, contrasena, confirmacionContrasena } =
+      req.body;
+    if (contrasena != confirmacionContrasena) {
+      throw new Error("Error 1");
+    }
+    res.send(req.body);
+  } catch (error) {
+    res.send(error.message);
+    console.log(error);
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
